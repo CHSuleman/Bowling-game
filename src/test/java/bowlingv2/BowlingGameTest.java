16a833e3ef4844e175505768711b95d5ee036161
Chaudhry Muhammad Suleman <c.suleman@ewo.com>
2024-07-09 20:37:29 +0200

    style: improved the tests names and added the display name

--- src/test/java/bowlingv2/BowlingGameTest.java
+++ src/test/java/bowlingv2/BowlingGameTest.java
@@ -1,15 +1,17 @@
 package bowlingv2;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BowlingGameTest {
+class BowlingGameTest {
 
     @Test
-     void testPrintRollsAndScores() {
+    @DisplayName("Test Rolls and Scores: Mixed Rolls")
+    void testRollsAndScores_MixedRolls() {
         List<Frame> frames = new ArrayList<>();
         frames.add(new Frame(1, 4));
         frames.add(new Frame(4, 5));
@@ -26,7 +28,8 @@ public class BowlingGameTest {
     }
 
     @Test
-    void testPrintRollsAndScoresCase2() {
+    @DisplayName("Test Rolls and Scores: Spares and Strikes")
+    void testRollsAndScores_SparesAndStrikes() {
         List<Frame> frames = new ArrayList<>();
         frames.add(new Frame(8, 1));
         frames.add(new Frame(6, 4));
@@ -39,11 +42,12 @@ public class BowlingGameTest {
         frames.add(new Frame(2, 0));
         frames.add(new Frame(5, 4, null));
 
-        Assertions.assertEquals(106 , BowlingGame.printRollsAndScores(frames));
+        Assertions.assertEquals(106, BowlingGame.printRollsAndScores(frames));
     }
 
     @Test
-    void testPrintRollsAndScoresCase3() {
+    @DisplayName("Test Rolls and Scores: Strikes and Spares")
+    void testRollsAndScores_StrikesAndSpares() {
         List<Frame> frames = new ArrayList<>();
         frames.add(new Frame(10, null));
         frames.add(new Frame(10, null));
@@ -56,11 +60,12 @@ public class BowlingGameTest {
         frames.add(new Frame(9, 0));
         frames.add(new Frame(10, 10, 7));
 
-        Assertions.assertEquals(160 , BowlingGame.printRollsAndScores(frames));
+        Assertions.assertEquals(160, BowlingGame.printRollsAndScores(frames));
     }
 
     @Test
-    void testPrintRollsAndScoresCase4() {
+    @DisplayName("Test Rolls and Scores: Perfect Game")
+    void testRollsAndScores_PerfectGame() {
         List<Frame> frames = new ArrayList<>();
         frames.add(new Frame(10, null));
         frames.add(new Frame(10, null));
@@ -77,7 +82,8 @@ public class BowlingGameTest {
     }
 
     @Test
-    void testPrintRollsAndScoresCase5() {
+    @DisplayName("Test Rolls and Scores: Gutter Game")
+    void testRollsAndScores_GutterGame() {
         List<Frame> frames = new ArrayList<>();
         frames.add(new Frame(0, 0));
         frames.add(new Frame(0, 0));
